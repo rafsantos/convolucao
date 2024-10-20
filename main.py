432010ad6309d095f86d3391c7adfd577a00478d
Rafael Santos <rlsantos@gmail.com>
2024-10-20 20:35:04 -0300

    layout melhorias na pagina

--- main.py
+++ main.py
@@ -9,25 +9,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
-      return render_template('img_in.html', pixHoriz=const.pixHoriz)
-    if request.method == 'POST':
-      #Recebendo nova URL da imagem
-      myurl = request.form['url']
-      #Recebendo nova definição de tamanho do processamento
-      try:
-        mypixHoriz = int(request.form['pixHoriz'])
-      except Exception as e:
-        mypixHoriz = const.pixHoriz
-      if  1 < mypixHoriz <= 30:
-        const.pixHoriz = mypixHoriz
-      else:
-        mypixHoriz = const.pixHoriz
-      
-      #Reprocessa a imagem com estes novos parâmetros
-      convolucao.novaImagem(myurl)
-      mensagem_imagem = "Link é de uma imagem" if const.url_valida else "Link não é imagem"
-      return render_template('img_in.html', url=myurl, pixHoriz=mypixHoriz, mensagem_imagem=mensagem_imagem)    
+  if request.method == 'GET':
+    return render_template('img_in.html', pixHoriz=const.pixHoriz)
+  if request.method == 'POST':
+    #Recebendo nova URL da imagem
+    myurl = request.form['url']
+    #Recebendo nova definição de tamanho do processamento
+    try:
+      mypixHoriz = int(request.form['pixHoriz'])
+    except Exception as e:
+      mypixHoriz = const.pixHoriz
+    if  1 < mypixHoriz <= 30:
+      const.pixHoriz = mypixHoriz
+    else:
+      mypixHoriz = const.pixHoriz
+    
+    #Reprocessa a imagem com estes novos parâmetros
+    convolucao.novaImagem(myurl)
+    mensagem_imagem = "Link é de uma imagem" if const.url_valida else "Link não é imagem"
+    return render_template('img_in.html', url=myurl, pixHoriz=mypixHoriz, mensagem_imagem=mensagem_imagem)
+  else:
+    render_template('img_in.html', pixHoriz=const.pixHoriz)
 
 if __name__ == '__main__':
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
